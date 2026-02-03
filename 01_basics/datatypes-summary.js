93b52e7f77f53f7f9177790876ae6ef647024e03
Kushal8085 <kushalpopli69@gmail.com>
2026-02-03 20:12:34 +0000

    memory

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -41,3 +41,26 @@ const myFunction = function(){
 console.log(typeof anotherId);
 
 
+// +++++++++++++++++++++++++++++++++++++++++++++
+
+// Stack (Primitive), Heap (Non - Primitive)
+
+let myYoutubename = "kushalpoplidotcom"
+
+let anothername = myYoutubename
+anothername = "chaiaurcode"
+
+// console.log(myYoutubename);
+// console.log(anothername);
+
+let userOne = {
+    email: "user@google.com",
+    upi: "user@ybl"
+}
+
+let userTwo = userOne
+
+userTwo.email = "kushal@google.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
