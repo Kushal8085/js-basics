8432cbe8f185cbf61c0be928b3b2a680cce00514
Kushal8085 <kushalpopli69@gmail.com>
2026-02-03 20:47:58 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,34 @@
+const name = "kushal"
+const repoCount = 50
+
+// console.log(name + repoCount + " Value");
+
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('kushal-kp-com')
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('s'));
+
+// const newString = gameName.substring(0, 4)
+// console.log(newString);
+
+// const anotherString = gameName.slice(-8, 4)
+// console.log(anotherString);
+
+// const newStringOne = "   kushal   "
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+const url = "https://kushal.com/kushal%20popli"
+
+console.log(url.replace('%20', '-'));
+
+console.log(url.includes('kushal'));
+console.log(url.includes('sundar'));
+console.log(gameName.split('-'));
